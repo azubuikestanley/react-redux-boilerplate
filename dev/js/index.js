2293e954aee6c9371fbda10b540eaaf2a6feb785
azubuikestanley <azubuikestanley@gmail.com>
2019-11-23 07:37:30 +0100

    added store and provider and connect my state

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,11 +1,18 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import allReducers from './reducers';
+import App from './components/app';
 
 
-const store = createStore();
+const store = createStore(allReducers);
 
 ReactDOM.render(
-  <h1>Hey there</h1>
+  <Provider store={store}> 
+    <App />
+  </Provider>
+ 
   , document.getElementById('root'));
+ 
