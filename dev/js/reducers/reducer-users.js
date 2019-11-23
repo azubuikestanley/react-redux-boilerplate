a60017072bcef5088a359e3e1df4847e92783e0a
azubuikestanley <azubuikestanley@gmail.com>
2019-11-23 08:47:53 +0100

    added actions and finished up

--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -1,25 +1,28 @@
 export default function() {
-    return [
-        {
-           id: 1,
-           first: "Stan",
-           last: "Iyke",
-           age: 14,
-           description: "Stan is a react developer",
-        },
-        {
-            id: 2,
-            first: "Stan2",
-            last: "Iyke3",
-            age: 15,
-            description: "Stan is a react developer",
-         },
-         {
-            id: 3,
-            first: "Stan3",
-            last: "Iyke3",
-            age: 16,
-            description: "Stan is a react developer",
-         }
-    ]
+   return [
+      {
+         id: 1,
+         first: "Stan",
+         last: "Iyke",
+         age: 14,
+         description: "Stan is a react developer",
+         thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+      },
+      {
+         id: 2,
+         first: "Stan2",
+         last: "Iyke3",
+         age: 15,
+         description: "Stan is a react developer",
+         thumbnail: "http://i.imgur.com/52xRlm8.png"
+      },
+      {
+         id: 3,
+         first: "Stan3",
+         last: "Iyke3",
+         age: 16,
+         description: "Stan is a react developer",
+         thumbnail: "http://i.imgur.com/4EMtxHB.png"
+      }
+   ]
 }
